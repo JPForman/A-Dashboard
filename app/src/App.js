312c296846d2c39f62e377cf7291696c6aa12596
Joshua Forman <cyborgforman@gmail.com>
2020-04-21 12:54:50 -0700

    wrap app in hashRouter

--- app/src/App.js
+++ app/src/App.js
@@ -14,6 +14,7 @@ function App() {
         setActiveView = {setActiveView}
         pageViewArray= {pageViewArray}
       />
+      
       <SplashPage
         activeView={activeView}
         setActiveView={setActiveView}
--- app/src/index.js
+++ app/src/index.js
@@ -20,7 +20,11 @@ import * as serviceWorker from './serviceWorker';
 </html> 
 */}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>, 
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
