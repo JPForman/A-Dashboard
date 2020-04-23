0b7f7d4789e4fec65f86ca1551169440e5c67697
Joshua Forman <cyborgforman@gmail.com>
2020-04-23 13:22:02 -0700

    modify router and include report variables

--- app/src/App.js
+++ app/src/App.js
@@ -16,6 +16,10 @@ import RequestAQuote from './components/ab-tests/submit-to-request-quote';
 import SidebarNav from './components/SidebarNav';
 import Error404 from './components/Error404';
 
+// Data constants
+import { reportVariables } from './constants/reportVariables.js'
+import { testVariables } from './constants/abTestVariables'
+
 function App() {
   const [activeView, setActiveView] = React.useState('Default');
   const pageViewArray = ['B2C Performance', 'Twilio', 'Coordinated Auction Insights', 'Get a Quote', 'Click to Call Sticky Footer', 'One Step Form - Retest', 'Submit to Request a Quote', 'B2A One Step']
@@ -50,27 +54,65 @@ function App() {
 
         <Route exact path='/b2cReport' render={()=><B2CReport 
           setSidebarNavDisplay= {setSidebarNavDisplay}
+          reportID = {reportVariables.reportIds.b2c}
           activeView = {activeView}/>}/>
 
         <Route exact path='/twilioReport' render={()=><TwilioReport 
+          reportID = {reportVariables.reportIds.twilio}
           setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
 
         <Route exact path='/agentReport' render={()=><AgentReport 
+          reportID = {reportVariables.reportIds.agent}
           setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
 
         <Route exact path='/getAQuote' render={()=><GetAQuote 
-         setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
+          title = {testVariables.report1.title}
+          description = {testVariables.report1.description}
+          startDate = {testVariables.report1.startDate}
+          endDate = {testVariables.report1.endDate}
+          cvr = {testVariables.report1.cvr}
+          statSig = {testVariables.report1.statSig}
+          dateOfStatSig = {testVariables.report1.dateOfStatSig}
+          setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
 
         <Route exact path='/clickToCall' render={()=><ClickToCall 
+          title = {testVariables.report2.title}
+          description = {testVariables.report2.description}
+          startDate = {testVariables.report2.startDate}
+          endDate = {testVariables.report2.endDate}
+          cvr = {testVariables.report2.cvr}
+          statSig = {testVariables.report2.statSig}
+          dateOfStatSig = {testVariables.report2.dateOfStatSig}
           setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
 
         <Route exact path='/oneStepForm' render={()=><OneStepForm 
+          title = {testVariables.report3.title}
+          description = {testVariables.report3.description}
+          startDate = {testVariables.report3.startDate}
+          endDate = {testVariables.report3.endDate}
+          cvr = {testVariables.report3.cvr}
+          statSig = {testVariables.report3.statSig}
+          dateOfStatSig = {testVariables.report3.dateOfStatSig}
           setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
 
         <Route exact path='/submitToRequestAQuote' render={()=><RequestAQuote 
+          title = {testVariables.report4.title}
+          description = {testVariables.report4.description}
+          startDate = {testVariables.report4.startDate}
+          endDate = {testVariables.report4.endDate}
+          cvr = {testVariables.report4.cvr}
+          statSig = {testVariables.report4.statSig}
+          dateOfStatSig = {testVariables.report4.dateOfStatSig}
           setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
 
         <Route exact path='/b2aOneStep' render={()=><B2AOneStep 
+          title = {testVariables.report5.title}
+          description = {testVariables.report5.description}
+          startDate = {testVariables.report5.startDate}
+          endDate = {testVariables.report5.endDate}
+          cvr = {testVariables.report5.cvr}
+          statSig = {testVariables.report5.statSig}
+          dateOfStatSig = {testVariables.report5.dateOfStatSig}
           setSidebarNavDisplay= {setSidebarNavDisplay}/>}/>
 
         <Route render={()=> <Error404 
