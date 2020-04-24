52b8df1ccd16f1186a57da7314151de16805c2a4
Joshua Forman <cyborgforman@gmail.com>
2020-04-24 13:50:45 -0700

    create master business reports list and refactor splash buttons as a loop

--- app/src/components/SplashPage.js
+++ app/src/components/SplashPage.js
@@ -3,58 +3,12 @@ import { Link } from 'react-router-dom';
 import Card from './Card';
 import GridHeading from './GridHeading';
 
-// Images
-import Twilio from '../media/AB-Tests.png';
-import Agent from '../media/Agent.png';
-import B2C from '../media/B2C.png';
-import ABTest from '../media/AB-Tests.png';
+// Reports List
 import masterABTestList from '../constants/masterABTestList';
+import masterBusinessReportsList from '../constants/masterBusinessReportsList';
 
-function SplashPage({ setActiveView, pageViewArray, setSidebarNavDisplay }) {
+function SplashPage({ setSidebarNavDisplay }) {
 
-  const dataList = [
-    {
-      header: pageViewArray[0],
-      imageURL: B2C,
-      info: 'Business to Consumer Data'
-    },
-    {
-      header: pageViewArray[1],
-      imageURL: Twilio,
-      info: 'Twilio Data'
-    },
-    {
-      header: pageViewArray[2],
-      imageURL: Agent,
-      info: 'Agent Data'
-    },
-    {
-      header: pageViewArray[3],
-      info: 'Get a Quote Data'
-    },
-    {
-      header: pageViewArray[4],
-      imageURL: ABTest,
-      info: 'Click to Call Sticky Footer'
-    },
-    {
-      header: pageViewArray[5],
-      imageURL: ABTest,
-      info: 'One Step Form - Retest'
-    },
-    {
-      header: pageViewArray[6],
-      imageURL: ABTest,
-      info: 'Submit to Request a Quote'
-    },
-    {
-      header: pageViewArray[7],
-      imageURL: ABTest,
-      info: 'B2A One Step'
-    }
-  ];
-
-  
   return (
   <div>
     {setSidebarNavDisplay('None')}
@@ -62,25 +16,15 @@ function SplashPage({ setActiveView, pageViewArray, setSidebarNavDisplay }) {
     <div className='reports'>
       <h1>Business Reports</h1>
       <div className="reports-business">
-        <Link to='/b2cReport' className='link'>
-          <Card
-            data={dataList[0]}
-            image={dataList[0].imageURL}
-          />
-        </Link>
-        <Link to='/twilioReport' className='link'>
-          <Card
-            data={dataList[1]}
-            image={dataList[1].imageURL}
-          />
-        </Link>
-        <Link to='/agentReport' className='link'>
-          <Card
-            data={dataList[2]}
-            image={dataList[2].imageURL}
-            setActiveView={setActiveView}
-          />
-        </Link>
+
+      {masterBusinessReportsList.map((test, index) => (
+          <Link to={test.lowerCamelCaseRouteTitle} className='link'>
+            <Card
+              title={test.title}
+              image={test.splashImgUrl}
+            />
+          </Link>
+        ))}
       </div>
 
       <h1>A/B Test Reports</h1>
